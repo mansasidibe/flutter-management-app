e45ba7734c25c5ac80bf456fe25adfd4fa3b127c
ronphobos <sidibearouna71@gmail.com>
2022-05-01 08:25:09 +0000

    installation package http request

--- lib/widgets/liste_transaction.dart
+++ lib/widgets/liste_transaction.dart
@@ -1,6 +1,8 @@
 import 'package:commerce/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+// import 'package:http/http.dart' as http;
+
 
 class TransationList extends StatelessWidget {
   final List<Transaction> transactions;
